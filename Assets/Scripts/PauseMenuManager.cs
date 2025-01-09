4f1e9907f1e762d14176b0ccd5f276190008e485
zxcpivo <tvo9mamma@gmail.com>
2025-01-08 19:40:24 -0500

    Updated pause menu

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public  GameObject pauseMenu;
+    public bool isPaused;
+
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Setting()
+    {
+
     }
 }
